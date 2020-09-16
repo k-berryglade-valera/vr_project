496b8308b31fe1c1aa9b35aa9543e272614e7552
k-berryglade-valera <kvyatkovskyproject@gmail.com>
2020-09-17 01:39:45 +0300

    Sensor for enemy

--- Entities/Enemy/Enemy.cs
+++ Entities/Enemy/Enemy.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 namespace VRApp.Entities.Enemy
 {
@@ -10,7 +13,35 @@ namespace VRApp.Entities.Enemy
         [SerializeField] private NavMeshAgent _navMeshAgent;
         [SerializeField] private float _speed;
         [SerializeField] private Transform _target;
+        [FormerlySerializedAs("_sensor")] [SerializeField] private List<Transform> _sensors;
         
+        private void ScanTarget()
+        {
+            SensorRaycast();
+        }
+
+        private void SensorRaycast()
+        {
+            RaycastHit info;
+            foreach (Transform sensor in _sensors)
+            {
+                
+                if (_target == null)
+                {
+                    if (Physics.Raycast(sensor.position,
+                        sensor.forward,
+                        out info,
+                        Mathf.Infinity)) 
+                    {
+                        if (info.transform.CompareTag("Player"))
+                        {
+                            _target = info.transform;
+                        }
+                    }
+                }
+                
+            }
+        }
         private bool SetName()
         {
 
@@ -21,6 +52,10 @@ namespace VRApp.Entities.Enemy
         {
             
         }
-        
+
+        private void Update()
+        {
+            ScanTarget();
+        }
     }
 }
