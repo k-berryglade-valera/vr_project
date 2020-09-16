45ccb826aa0c7601173c1210ae418d1ce8d645fc
k-berryglade-valera <kvyatkovskyproject@gmail.com>
2020-09-17 01:13:48 +0300

    Enemy prototype

--- Entities/Enemy/Enemy.cs
+++ Entities/Enemy/Enemy.cs
@@ -1,7 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
 namespace VRApp.Entities.Enemy
 {
+    // PROTOTYPE
     public class Enemy : EnemyBehaviour
     {
+
+        [SerializeField] private NavMeshAgent _navMeshAgent;
+        [SerializeField] private float _speed;
+        [SerializeField] private Transform _target;
+        
+        private bool SetName()
+        {
+
+            return true;
+        }
+
+        private void Move()
+        {
+            
+        }
         
     }
 }
